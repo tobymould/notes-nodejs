4f680e19cb2ea9297712661e551f3d7df34d15e9
tobymould <toby.mould@icloud.com>
2020-09-10 12:00:07 +0100

    functional notes app that uses CLI inputs to add and remove notes

--- notes.js
+++ notes.js
@@ -3,8 +3,9 @@ const chalk = require('chalk');
 
 const addNote = (title, body) => {
   const notes = loadNotes();
-
   const duplicateNote = notes.find(note => note.title === title);
+
+  debugger;
 
   if (!duplicateNote) {
     notes.push({ title: title, body: body });
